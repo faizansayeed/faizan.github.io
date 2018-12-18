8e72d4a7a9c8c4fa1b15783a64d126b2325d2c06
faizansayeed <faizansayeed@gmail.com>
2018-12-17 22:30:20 -0500

    update

---
+++ resumeX-master/src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'React';
+
+const AppFooter = () => (
+  <footer>
+    Elevenbeans' site. Inspired by&nbsp;
+    <a
+      href = "https://github.com/Ovilia"
+      target = "_blank"
+    >
+      Ovilia
+    </a>
+  </footer>
+);
+
+export default AppFooter;
